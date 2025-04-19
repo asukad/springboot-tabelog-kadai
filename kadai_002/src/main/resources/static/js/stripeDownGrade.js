5c77952ce0c5da299420d5c73028b1a77b918a8d
asukad <166801483+asukad@users.noreply.github.com>
2025-04-19 16:12:19 +0900

    Update stripeDownGrade.js

--- kadai_002/src/main/resources/static/js/stripeDownGrade.js
+++ kadai_002/src/main/resources/static/js/stripeDownGrade.js
@@ -1,20 +1,20 @@
-const stripe = Stripe('pk_test_51PZr0JLJ2LcWpnob4luZBrJD98cHEpLTAAelVVkNEnMIh6ccM0AWvTYjZYQdqaGB4NDX6K49ol4HynL66ijeO91p00UClyBd21');
-const cancelButton = document.querySelector('#cancelButton');
+// const stripe = Stripe('pk_test_51PZr0JLJ2LcWpnob4luZBrJD98cHEpLTAAelVVkNEnMIh6ccM0AWvTYjZYQdqaGB4NDX6K49ol4HynL66ijeO91p00UClyBd21');
+// const cancelButton = document.querySelector('#cancelButton');
 
-cancelButton.addEventListener('click', () => {
+// cancelButton.addEventListener('click', () => {
 	
-	fetch('/user/cancel', {
-	    method: 'POST'
-	})
-	.then(response => response.text())
-	.then(data => {
-	    if (data.startsWith('Error')) {
-	        alert('Failed: ' + data); // エラーの詳細を表示
-	    } else {
-	        alert(data); // 成功メッセージを表示
-	    }
-	})
-	.catch(error => alert('Network Error: ' + error));
+// 	fetch('/user/cancel', {
+// 	    method: 'POST'
+// 	})
+// 	.then(response => response.text())
+// 	.then(data => {
+// 	    if (data.startsWith('Error')) {
+// 	        alert('Failed: ' + data); // エラーの詳細を表示
+// 	    } else {
+// 	        alert(data); // 成功メッセージを表示
+// 	    }
+// 	})
+// 	.catch(error => alert('Network Error: ' + error));
 
-});
+// });
 		 
